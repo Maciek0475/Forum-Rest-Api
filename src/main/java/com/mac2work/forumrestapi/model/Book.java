590f413b55e479dcfc4ad7697f0856105340482b
Maciej Jurczak <mac.jurczak@o2.pl>
2023-05-07 20:42:14 +0200

    adding empty models

---
+++ src/main/java/com/mac2work/forumrestapi/model/Book.java
@@ -0,0 +1,14 @@
+package com.mac2work.forumrestapi.model;
+
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@Entity(name="")
+public class Book {
+
+}
