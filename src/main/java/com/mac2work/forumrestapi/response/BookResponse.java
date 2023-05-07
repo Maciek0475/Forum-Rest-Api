49152b3671ae59f105e3b1502cf88940415d7032
Maciej Jurczak <mac.jurczak@o2.pl>
2023-05-07 22:29:18 +0200

    adding book payload

---
+++ src/main/java/com/mac2work/forumrestapi/response/BookResponse.java
@@ -0,0 +1,17 @@
+package com.mac2work.forumrestapi.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookResponse {
+
+	private String name;
+	private Integer publication_year;
+	private String description;
+}
