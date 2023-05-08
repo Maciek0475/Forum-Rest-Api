6f6e8bef26bdfd45c785da1fd7e640a9953c9e00
Maciej Jurczak <mac.jurczak@o2.pl>
2023-05-08 23:10:58 +0200

    Adding get methods to BookController

--- src/main/java/com/mac2work/forumrestapi/service/BookService.java
+++ src/main/java/com/mac2work/forumrestapi/service/BookService.java
@@ -1,11 +1,26 @@
 package com.mac2work.forumrestapi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.mac2work.forumrestapi.model.Book;
+import com.mac2work.forumrestapi.repository.BookRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
+	
+	private final BookRepository bookRepository;
+	
+	public List<Book> getBooks() {
+		return bookRepository.findAll();
+	}
+
+	public Book getSpecificBook(Integer id) {
+		return bookRepository.findById(id).orElseThrow();
+	}
 
 }
