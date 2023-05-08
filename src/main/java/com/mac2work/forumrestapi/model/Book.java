75329329901af5efef35038403cb0e9b1592cb2d
Maciej Jurczak <mac.jurczak@o2.pl>
2023-05-08 23:23:26 +0200

    Updating Book

--- src/main/java/com/mac2work/forumrestapi/model/Book.java
+++ src/main/java/com/mac2work/forumrestapi/model/Book.java
@@ -1,6 +1,12 @@
 package com.mac2work.forumrestapi.model;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +14,17 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
-@Entity(name="")
+@AllArgsConstructor
+@Entity(name="books")
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name = "native", strategy = "native")
+	private Integer id;
+	private String name;
+	private Integer publicationYear;
+	private String description;
+
 
 }
