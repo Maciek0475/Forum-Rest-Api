fb1da25d8f284eb466d316bd52afc27ed05fb8d9
Maciej Jurczak <mac.jurczak@o2.pl>
2023-06-14 15:55:13 +0200

    Creating isCorrectUser authorization

---
+++ src/main/java/com/mac2work/forumrestapi/exception/IncorrectUserException.java
@@ -0,0 +1,16 @@
+package com.mac2work.forumrestapi.exception;
+
+public class IncorrectUserException extends RuntimeException {
+	
+	private String resource;
+	private String message;
+	public IncorrectUserException(String resource) {
+		this.resource = resource;
+		formatMessage();
+	}
+	
+	private void formatMessage() {
+		this.message = String.format("You have no rights to modify this %s", resource);
+	}
+
+}
--- src/main/java/com/mac2work/forumrestapi/service/AuthorizationService.java
+++ src/main/java/com/mac2work/forumrestapi/service/AuthorizationService.java
@@ -4,22 +4,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.mac2work.forumrestapi.exception.IncorrectUserException;
 import com.mac2work.forumrestapi.exception.NoAccessException;
+import com.mac2work.forumrestapi.model.User;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class AuthorizationService {
 	
+	private final UserService userService;
+	
 	public boolean isAdmin(String path, String mappingMethod) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		boolean isAdmin = auth.getAuthorities().stream()
-			.filter(a -> a.toString() == "ADMIN")
-			.findAny().orElseThrow(
-					() -> new NoAccessException(path, mappingMethod, "ADMIN")) != null;
+		User user = userService.getLoggedInUser();
+		boolean isAdmin = user.getRole().toString() == "ADMIN";
+		if(!isAdmin)
+			throw new NoAccessException(path, mappingMethod, "ADMIN");
+		return isAdmin;
+	}
+	private boolean isAdmin(User user) {
+		boolean isAdmin = user.getRole().toString() == "ADMIN";
+		return isAdmin;
+	}
+	
+	public boolean isCorrectUser(Integer id, String resource) {
 		
-		if(isAdmin)
-			return true;
-		else
-			return false;
+		User user = userService.getLoggedInUser();
+		if(isAdmin(user))
+		return true;
+		boolean isCorrectUser = user.getId() == id;
+		if(!isCorrectUser)
+			throw new IncorrectUserException(resource);
+		return isCorrectUser;
+
 	}
 
 }
